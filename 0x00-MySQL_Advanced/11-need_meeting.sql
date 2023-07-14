2b1471b125662ee518a8c9b56149dbb88adf6262
Kelvin <edithmukoro3@gmail.com>
2023-07-14 03:31:35 +0100

    11-fix

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,12 +2,13 @@
 -- the first by the second number or returns 0
 -- if the second number is equal to 0.
 
+-- Change the delimiter to $$
 DELIMITER $$
-DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS FLOAT
-BEGIN
-    RETURN (IF (b = 0, 0, a / b));
-END
+-- Create the view need_meeting
+CREATE VIEW need_meeting AS
+SELECT student_name
+FROM students
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
 $$
+-- Reset the delimiter back to ;
 DELIMITER ;
