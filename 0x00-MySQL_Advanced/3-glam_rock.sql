7e3ee476a02cef20cac554552dc21cf9786ae33c
Kelvin <edithmukoro3@gmail.com>
2023-07-14 03:28:22 +0100

    advance task

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,20 @@
 -- ranked by their longevity
 -- Column names must be: band_name & lifespan
 
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+-- Create a temporary table to store the bands and their lifespan
+CREATE TEMPORARY TABLE temp_bands (
+  band_name VARCHAR(100),
+  lifespan INT
+);
+
+-- Insert the bands and their lifespan into the temporary table
+INSERT INTO temp_bands (band_name, lifespan)
+SELECT band_name,
+       YEAR("2022-01-01") - YEAR(FROM_UNIXTIME(UNIX_TIMESTAMP(split, 'dd.mm.yyyy'))) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+WHERE main_style = 'Glam rock';
+
+-- List the bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, lifespan
+FROM temp_bands
 ORDER BY lifespan DESC;
