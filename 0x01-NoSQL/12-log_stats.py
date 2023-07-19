f1204bc862b5778419d2f0d86c96823073e80393
Kelvin <edithmukoro3@gmail.com>
2023-07-19 03:00:30 +0100

    task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+A Python module tha provides stats about nginx
+"""
+
+
+from pymongo import MongoClient
+"""
+Prints the log stats in nginx collection
+"""
+con = MongoClient("mongodb://localhost:27017")
+
+collection = con.logs.nginx
+
+print(f"{collection.estimated_document_count()} logs")
+
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+print("Methods:")
+
+for req in methods:
+    print("\tmethods {}: {}".format(req,
+                                    collection.count_documents({"method": req})))
+print("{} status check".format
+      (collection.count_documents
+       ({"method": "GET", "path": '/status'})))
+
