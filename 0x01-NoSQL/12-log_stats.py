d0460c74ff21dc92975d3807c01bd8c3dac1ef5e
Kelvin <edithmukoro3@gmail.com>
2023-07-19 03:04:03 +0100

    task 12 -fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
-"""
-A Python module tha provides stats about nginx
-"""
 
+'''A Python module tha provides stats about nginx'''
 
 from pymongo import MongoClient
-"""
-Prints the log stats in nginx collection
-"""
-con = MongoClient("mongodb://localhost:27017")
 
-collection = con.logs.nginx
 
-print(f"{collection.estimated_document_count()} logs")
+if __name__ == '__main__':
+    '''Prints the log stats in nginx collection'''
+    con = MongoClient('mongodb://localhost:27017')
+    collection = con.logs.nginx
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print("Methods:")
+    print(f'{collection.estimated_document_count()} logs')
 
-for req in methods:
-    print("\tmethods {}: {}".format(req,
-                                    collection.count_documents({"method": req})))
-print("{} status check".format
-      (collection.count_documents
-       ({"method": "GET", "path": '/status'})))
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print('Methods:')
+
+    for req in methods:
+        print('\tmethods {}: {}'.format(req,
+              collection.count_documents({'method': req})))
+
+    print('{} status check'.format(collection.count_documents(
+          {'method': 'GET', 'path': '/status'})))
 
